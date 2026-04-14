9920bec3541df428cde5b869a93f8abdc92977fe
Philip Helger <philip@helger.com>
2026-04-14 08:56:34 +0200

    Renamed

--- src/test/java/com/example/peppol/mcp/CPhossPeppolMcpTest.java
+++ src/test/java/com/example/peppol/mcp/CPhossPeppolMcpTest.java
@@ -26,7 +26,7 @@ import org.junit.Test;
  *
  * @author Philip Helger
  */
-public final class CPhossPeppolMCPVersionTest
+public final class CPhossPeppolMcpTest
 {
   @Test
   public void testBasic ()
